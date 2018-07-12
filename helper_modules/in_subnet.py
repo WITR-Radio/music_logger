fb150f190363808d4a0d603056c1162147a01383
Colin Reilly <colinrlly@gmail.com>
2018-07-12 15:51:03 -0400

    Added back in subnet checks

--- helper_modules/in_subnet.py
+++ helper_modules/in_subnet.py
@@ -4,5 +4,4 @@ def in_subnet(addr):
     """ Simple function to return True if IP is in the WITR subnet
         and False if it is not
     """
-    return True
-    # return IPAddress(addr) in IPNetwork('129.21.97.0/26')
+    return IPAddress(addr) in IPNetwork('129.21.97.0/26')

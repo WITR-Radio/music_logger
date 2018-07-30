6173c4e0f43f1e17467f3c6ec043b125a83f8e53
Colin Reilly <colinrlly@gmail.com>
2018-07-30 13:33:22 -0400

    Added back subnet checks

--- static/logger/js/in_subnet.js
+++ static/logger/js/in_subnet.js
@@ -2,11 +2,11 @@
  *      variable so other js files can easily check
  *      if this client is in the subnet or not ***/
 
-var in_subnet = true;
+var in_subnet;
 
-// $.get('/is_in_subnet', function(data) {
-//     if (data === 'False')
-//         window.in_subnet = false;
-//     else
-//         window.in_subnet = true;
-// });
+$.get('/is_in_subnet', function(data) {
+    if (data === 'False')
+        window.in_subnet = false;
+    else
+        window.in_subnet = true;
+});
